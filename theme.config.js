6ae006bc739e07bc4ce5a1d99ea4a04fc8d7252e
John Cantrell <johncantrell97@gmail.com>
2022-01-22 16:01:19 -0500

    clean up theme.config.js a bit

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  github: 'https://github.com/L2-Technology/docs',
+  docsRepositoryBase: 'https://github.com/L2-Technology/docs/blob/main',
+  titleSuffix: ' – Sensei',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">Sensei</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        Enterprise-grade lightning network implementation
       </span>
     </>
   ),
@@ -16,15 +16,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="Sensei Documentation: an enterprise-grade lightning node implementation" />
+      <meta name="og:description" content="Sensei Documentation: an enterprise-grade lightning node implementation" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
+      <meta name="twitter:site:domain" content="docs.l2.technology" />
+      <meta name="twitter:url" content="https://docs.l2.technology" />
+      <meta name="og:title" content="Sensei Documentation: an enterprise-grade lightning node implementation" />
       <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="apple-mobile-web-app-title" content="Sensei Documentation" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -62,6 +62,6 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <>L2 Technology © {new Date().getFullYear()}</>,
   unstable_faviconGlyph: '👋',
 }
